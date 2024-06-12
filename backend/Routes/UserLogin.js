2ab217a75c0e61bf93d5f1b0842ac07c7124ff2a
AjayTumula <ajaytumula72@gmail.com>
2024-06-12 23:29:45 +0530

    Created the dashboard component and implemented the styling, routing in app.jsx

--- backend/Routes/UserLogin.js
+++ backend/Routes/UserLogin.js
@@ -9,9 +9,7 @@ router.post('/login', (req, res) => {
     const sql = "SELECT * from user_login Where email = ? and password = ?";
     connection.query(sql, [req.body.email, req.body.password], (err, result) => {
         
-        if(err) return res.status(500).json({ loginStatus: false, msg: "Connection failed" })
-         
-          
+        if(err) return res.status(500).json({ loginStatus: false, Error: "Connection failed" })
             if(result.length > 0) {
                 const email = result[0].email;
                 const token = jwt.sign({role: "user", email: email, id: result[0].id}, "jwt_key", {expiresIn: "1d"});
@@ -20,7 +18,7 @@ router.post('/login', (req, res) => {
             } else {
                 return res.json({
                     loginStatus: false,
-                    msg: "Wrong credentials"
+                    Error: "Wrong credentials"
                 })
             }
         
